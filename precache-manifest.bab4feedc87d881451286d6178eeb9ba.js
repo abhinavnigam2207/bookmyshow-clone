6c03d5a09e941e5386f604ef291ee2f78fb84376
Abhinav Nigam <abhinavnigam2207@gmail.com>
2020-08-31 06:04:21 +0530

    Updates

---
+++ precache-manifest.bab4feedc87d881451286d6178eeb9ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38c22f17d221a08222f32b4b7f31aa27",
+    "url": "/bookmyshow-clone/index.html"
+  },
+  {
+    "revision": "c99e76c03c1b5d847b4a",
+    "url": "/bookmyshow-clone/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5c88f9e449aac156f3c4",
+    "url": "/bookmyshow-clone/static/js/2.0dd332ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bookmyshow-clone/static/js/2.0dd332ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c99e76c03c1b5d847b4a",
+    "url": "/bookmyshow-clone/static/js/main.2ed1dacc.chunk.js"
+  },
+  {
+    "revision": "fb8e228cdbd774e6232e",
+    "url": "/bookmyshow-clone/static/js/runtime-main.5a837911.js"
+  }
+]);
